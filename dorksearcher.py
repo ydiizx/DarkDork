3cb74d2c8e9c3cd01fe44cc2f4c911b28164157a
ydiizx <59018383+ydiizx@users.noreply.github.com>
2021-07-23 09:00:32 +0200

    res

--- dorksearcher.py
+++ dorksearcher.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 from random import choice
 from urllib.parse import urlparse
 from os import system, name
-from treq import get
 
 import re
 import threading
@@ -136,7 +135,7 @@ def worker_yahoo(query, ua, prox):
 	pages = None
 	r = None
 	try:
-		r = get(YAHOO_URL+query, headers=ua, proxies=prox, timeout=10)
+		r = requests.get(YAHOO_URL+query, headers=ua, proxies=prox, timeout=10)
 		if r.status_code == 500:
 			print("Got blocked error!!")
 			return
@@ -161,7 +160,7 @@ def worker_yahoo(query, ua, prox):
 	if pages:
 		for page in pages:
 			try:
-				r = get(page, headers=ua).text
+				r = requests.get(page, headers=ua).text
 				parsing_yahoo(r, method='yahoo')
 			except:
 				continue
